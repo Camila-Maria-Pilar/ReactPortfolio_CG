0ee1ae5164a07f19dd8bf91731ff67535450cb96
Camila Galleguillos <camila.m.galleguillos@gmail.com>
2023-08-10 20:47:13 +0800

    resume section fixes-resume pdf added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,34 +2,43 @@ import styles from '../styles/Resume.module.css';
 
 import React from 'react';
 
+
 function Resume() {
     return (
         <section className={styles.Resume}>
-            {/* <a href="pending to update" target="_blank" rel="noopener noreferrer">Download My Resume</a>*/}
-            <h2>Knowledge in:</h2>
-            <ul>
-                <li>HTML-CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Node.js</li>
-                <li>Express.js</li>
-                <li>MySQL</li>
-                <li>MongoDB</li>
-                <li>Proficient in Project Scope Management</li>
-                
-            </ul>
+            <button>
+                <a href="./documents/CGResume.pdf" download target="_blank" rel="noopener noreferrer">Download My Resume</a>
+            </button>
 
-            <h2>My Professional Background:</h2>
-            <ul>
-                <li>Industrial Engineer</li>
-                <li>MBA</li>
-                <li>Entrepreneur</li> 
-                <li>Business Development</li>                                              
-            </ul>
+            <div className={styles.section}>
+                <h2>Knowledge in:</h2>
+                <ul>
+                    <li><span className={styles.iconHTML}></span>HTML-CSS</li>
+                    <li><span className={styles.iconJS}></span>JavaScript</li>
+                    <li><span className={styles.iconReact}></span>React</li>
+                    <li><span className={styles.iconNode}></span>Node.js</li>
+                    <li><span className={styles.iconNode}></span>Express.js</li>
+                    <li><span className={styles.iconNode}></span>MySQL</li>
+                    <li><span className={styles.iconNode}></span>MongoDB</li>
+                    <li><span className={styles.iconNode}></span>Proficient in Project Scope Management</li>                                  
+               
+                </ul>
+            </div>
 
+            <div className={styles.section}>
+                <h2>My Professional Background:</h2>
+                <ul>
+                    <li><span className={styles.iconEngineer}></span>Industrial Engineer</li>
+                    <li><span className={styles.iconMBA}></span>MBA</li>
+                    <li><span className={styles.iconEntrepreneur}></span>Entrepreneur</li>
+                    <li><span className={styles.iconBusinessDevelopment}></span>Business Development</li>
+                </ul>
+            </div>
         
         </section>
     );
 }
+
+                               
 
 export default Resume;
